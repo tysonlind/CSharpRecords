460c32f27a68152fdcec0f4ed31cb1bc3bd89541
tyson lind <tysonfloydlind@gmail.com>
2024-10-31 17:00:23 -0600

    initial commit for student exercise repository

---
+++ CSharpRecords/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CSharpRecords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a new Contact.cs file that defines a record type called Contact
+
+
+            // Create some new Contact instances here
+
+            //Display your contacts to the console with Console.WriteLine();
+
+            //BONUS: Add a ContactBook class with a method to add contacts to a list and a method to print out each contact.
+
+            //BONUS2: Use the value-based equality feature of records by checking if the contact book already contains a contact before adding it to the list.
+        }
+    }
+}
